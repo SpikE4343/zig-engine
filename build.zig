d6a38579abd2c17caf349c74625729015584f731
spike4343 <johnrohrssen@gmail.com>
2020-10-10 15:15:56 -0700

    setup to build windows

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const os = std.os;
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
@@ -8,9 +9,15 @@ pub fn build(b: *Builder) void {
     // const libgame = b.addSharedLibrary("game", "src/game/game_cubes.zig", b.version(0, 1, 0));
 
     // exe.linkLibrary(libgame);
-
+    //exe.setBuildMode(mode);
     //exe.setBuildMode(std.builtin.Mode.Debug);
-    exe.linkSystemLibrary("SDL2");
+
+    if(std.builtin.os.tag == .windows) {
+        exe.addIncludeDir("external/SDL2-2.0.12/include");
+        exe.linkSystemLibrary("external/SDL2-2.0.12/lib/x64/SDL2");
+    } else {    
+        exe.linkSystemLibrary("SDL2");
+    }
     exe.linkSystemLibrary("c");
 
     //b.default_step.dependOn(&libgame.step);
